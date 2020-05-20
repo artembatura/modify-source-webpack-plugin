3e292819c7ae83d21f0f37f88fe42470a197a060
Artem Batura <artemir.q@gmail.com>
2020-05-20 22:24:29 +0300

    Maintenance

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { compilation, Compiler } from 'webpack';
 import path from 'path';
+// eslint-disable-next-line import/no-unresolved
+import type { compilation, Compiler } from 'webpack';
 
 export interface Option {
   test: RegExp | ((module: compilation.Module | any) => boolean);
@@ -13,31 +14,34 @@ export class ModifyModuleSourcePlugin {
   constructor(private readonly options: Options) {}
 
   public apply(compiler: Compiler): void {
-    compiler.hooks.compilation.tap(this.constructor.name, compilation => {
-      compilation.hooks.finishModules.tap(
-        this.constructor.name,
-        (modules: any[]) => {
-          const options: Option[] = [].concat(this.options as never[]);
+    compiler.hooks.compilation.tap(
+      'ModifyModuleSourcePlugin',
+      (compilation) => {
+        compilation.hooks.finishModules.tap(
+          'ModifyModuleSourcePlugin',
+          (modules: any[]) => {
+            const options: Option[] = [].concat(this.options as never[]);
 
-          options.forEach(({ test, modify, findFirst }) => {
-            for (const module of Object.values(modules)) {
-              if (
-                (typeof test === 'function' && test(module)) ||
-                (test instanceof RegExp && test.test(module.userRequest))
-              ) {
-                module._source._value = modify(
-                  module._source._value,
-                  path.basename(module.userRequest)
-                );
+            options.forEach(({ test, modify, findFirst }) => {
+              for (const module of Object.values(modules)) {
+                if (
+                  (typeof test === 'function' && test(module)) ||
+                  (test instanceof RegExp && test.test(module.userRequest))
+                ) {
+                  module._source._value = modify(
+                    module._source._value,
+                    path.basename(module.userRequest)
+                  );
 
-                if (findFirst) {
-                  break;
+                  if (findFirst) {
+                    break;
+                  }
                 }
               }
-            }
-          });
-        }
-      );
-    });
+            });
+          }
+        );
+      }
+    );
   }
 }
