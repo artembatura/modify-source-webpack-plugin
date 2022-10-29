d2896379891a8b60856b3b1bed0c4cbdd3296b59
Artem Batura <artemir.q@gmail.com>
2022-10-29 23:46:09 +0300

    Update Vue example

--- examples/modify-vue-files/vue.config.js
+++ examples/modify-vue-files/vue.config.js
@@ -1,4 +1,4 @@
-const { ModifySourcePlugin } = require('modify-source-webpack-plugin');
+const { ModifySourcePlugin, ReplaceOperation } = require('modify-source-webpack-plugin');
 
 module.exports = {
   configureWebpack: {
@@ -6,10 +6,16 @@ module.exports = {
       new ModifySourcePlugin({
         rules: [
           {
-            test: /App\.vue\?vue.*/,
-            modify: (src) => {
-              return src.replace('Welcome to Your Vue.js App', 'Welcome to the club buddy');
-            }
+            test: /App\.vue\?vue&type=template.*/,
+            operations: [
+              new ReplaceOperation('all', 'Welcome to Your Vue.js App', 'Welcome to the club buddy')
+            ]
+          },
+          {
+            test: /App\.vue\?vue&type=script.*/,
+            operations: [
+              new ReplaceOperation('once', '// {MY_CUSTOM_CODE}', '\nconsole.log(`filepath: $FILE_PATH\nfilename: $FILE_NAME`)')
+            ]
           }
         ]
       })
