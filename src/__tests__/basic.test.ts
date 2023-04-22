c8fbef91628511a376ed96c0b22115aa441661c2
Artem Batura <artembatura.dev@gmail.com>
2023-04-23 02:36:06 +0300

    Remove `pnp-webpack-plugin` package

--- src/__tests__/basic.test.ts
+++ src/__tests__/basic.test.ts
@@ -7,8 +7,6 @@ import webpackV4 from 'webpack-v4';
 import { ModifySourcePlugin } from '../ModifySourcePlugin';
 import { ConcatOperation } from '../operations';
 
-const PnpWebpackPlugin = require('pnp-webpack-plugin');
-
 import DoneCallback = jest.DoneCallback;
 
 const OUTPUT_PATH = path.resolve(__dirname, './build/basic-test');
@@ -388,17 +386,7 @@ function runTests(webpack: typeof webpackV4 | typeof webpackV5) {
               )
             }
           ]
-        },
-        ...(webpack === webpackV4
-          ? {
-              resolve: {
-                plugins: [PnpWebpackPlugin]
-              },
-              resolveLoader: {
-                plugins: [PnpWebpackPlugin.moduleLoader(module)]
-              }
-            }
-          : {})
+        }
       },
       done,
       [],
@@ -438,17 +426,7 @@ function runTests(webpack: typeof webpackV4 | typeof webpackV5) {
               )
             }
           ]
-        },
-        ...(webpack === webpackV4
-          ? {
-              resolve: {
-                plugins: [PnpWebpackPlugin]
-              },
-              resolveLoader: {
-                plugins: [PnpWebpackPlugin.moduleLoader(module)]
-              }
-            }
-          : {})
+        }
       },
       done,
       [],
